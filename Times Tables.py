d35a03e9abfadf8848551f37975d3a9e94f679c0
AkairoShikkoku <anh.ho2002@gmail.com>
2017-09-20 16:07:52 +0930

    Add files via upload

---
+++ Times Tables.py
@@ -0,0 +1,39 @@
+def timestables():
+    for i in range(1,13):
+        print(i, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*2, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*3, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*4, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*5, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*6, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*7, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*8, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*9, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*10, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*11, end=' ')
+    print ()
+    for i in range(1,13):
+        print(i*12, end=' ')
+    print ()
+
+timestables()
